c2cd492459c9ed71955a27e6b66b4f30a313e173
Mahlatsi13 <tebomahlatsi@yahoo.co.za>
2023-07-19 07:37:56 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
-int _islower(int c)
+void print_alphabet_10_times(void)
 {
-    return (c >= 'a' && c <= 'z') ? 1 : 0;
+    char letter = 'a';
+
+    for (int i = 0; i < 10; i++)
+    {
+        while (letter <= 'z')
+        {
+            putchar(letter);
+            letter++;
+        }
+
+        putchar('\n');
+        letter = 'a'; // Reset letter to 'a' for the next iteration
+    }
 }
+
+int main()
+{
+    print_alphabet_10_times();
+    return 0;
+}
+
