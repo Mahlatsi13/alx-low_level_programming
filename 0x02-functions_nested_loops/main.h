cda148b03b11ce4484153132b04ce592e9c9a469
Mahlatsi13 <tebomahlatsi@yahoo.co.za>
2023-07-19 07:31:43 -0700

    functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,13 @@ int _putchar(char c);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-void print_alphabet_x10(void)
- 
+int _abs(int num);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+
+
 #endif /* MAIN_H */
