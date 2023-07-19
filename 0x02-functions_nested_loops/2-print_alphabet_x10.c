ec14568e8f3aaa1256a0a691cba0997a5e74a30f
Mahlatsi13 <tebomahlatsi@yahoo.co.za>
2023-07-19 07:13:03 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
-void print_alphabet_x10(void)
+int _islower(int c)
 {
-	char ch;
-	int i;
-
-	i = 0;
-
-	while (i < 10)
-	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++;
-	}
+    return (c >= 'a' && c <= 'z') ? 1 : 0;
 }
