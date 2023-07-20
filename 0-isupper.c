bc7e71d8e9ab45402e5d0819b1358c6223ba182d
Mahlatsi13 <tebomahlatsi@yahoo.co.za>
2023-07-20 07:37:49 -0700

    loops

---
+++ 0-isupper.c
@@ -0,0 +1,14 @@
+// is_uppercase.c
+
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
+
